df10a00f2a277d5b4a9353e6ec0fb910dfdf49bc
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-02-03 23:04:01 +0100

    Initial Commit

---
+++ TheSign/TheSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSignApp.swift
+//  TheSign
+//
+//  Created by Antonio Emanuele Cutarella on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TheSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
